24f7d92ca4eb78306eca511819d2121cefb1fcb6
karenandrea1369 <karenandrea12204@gmail.com>
2019-09-20 08:45:14 -0500

    ya gira random

--- script/main.js
+++ script/main.js
@@ -1,10 +1,18 @@
 function handleLoad(){
-    
-    var numRan = Math.floor(Math.random()*(360));
-    var arro = document.querySelector( '.poten__arrow');
-    
-    arro.style.transform = 'rotate(' + numRan + 'deg)';
-    console.log(numRan);
+
+    function handleRandom(){
+        var numRan = Math.floor(Math.random()*(360));
+        var arro = document.querySelector( '.poten__arrow');
+        
+        arro.style.transform = 'rotate(' + numRan + 'deg)';
+        console.log(numRan);
+    }
+
+
+    setInterval(handleRandom, 2000);
+
 }
+
+
 
 window.addEventListener('load', handleLoad);
