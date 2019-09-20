ef3a63ef3c0087f43674f922c2ede3a730474bca
karenandrea1369 <karenandrea12204@gmail.com>
2019-09-20 08:56:42 -0500

    con los grados

--- script/main.js
+++ script/main.js
@@ -3,8 +3,9 @@ function handleLoad(){
     function handleRandom(){
         var numRan = Math.floor(Math.random()*(360));
         var arro = document.querySelector('.poten__arrow');
+        var text = document.querySelector('.grades');
         arro.style.transform = 'rotate(' + numRan + 'deg)';
-        console.log(numRan);
+        text.textContent = numRan;
     }
 
     setInterval(handleRandom, 2000);
